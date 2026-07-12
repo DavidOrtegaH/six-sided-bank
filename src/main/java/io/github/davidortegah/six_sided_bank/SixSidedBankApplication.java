abe3b6cc5b003ab604e813770f528e3adf72698f
davidortegah <davidortegahoyas@gmail.com>
2026-07-12 19:41:40 +0200

    chore: proyecto inicializado

---
+++ src/main/java/io/github/davidortegah/six_sided_bank/SixSidedBankApplication.java
@@ -0,0 +1,13 @@
+package io.github.davidortegah.six_sided_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SixSidedBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SixSidedBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/davidortegah/six_sided_bank/SixSidedBankApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.davidortegah.six_sided_bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SixSidedBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
